e4832f2ebb9e980c47cbe25df5fa8a62ec5440cc
Shen <oceanink2718@gmail.com>
2026-02-10 18:44:19 +0100

    xxz chain + crosscap overlay

--- examples/xxz_chain_crosscap.jl
+++ examples/xxz_chain_crosscap.jl
@@ -1,28 +1,48 @@
 using MyDMRGPkg
+using TensorOperations
 using Plots, Printf
 
-function xxz_crosscap_overlay_exact(Δ::Float64)
+function xxz_luttinger_exact(Δ::Float64)
     @assert -1 < Δ && Δ < 1
     K = pi / (2 * (pi - acos(Δ)))
-    return 1 / sqrt(K)
+    return K
 end
 
+function xxz_crosscap_overlay(mps::MPS)
+    N = mps.N
+    @assert N % 4 == 0
 
-function xxz_crosscap_overlay(mps::MPS, Δ::Float64)
-    @assert -1 < Δ && Δ < 1
-
+    half_N = div(N, 2)
+    A1 = mps.A[1][1, :, :]
+    C = nothing
+    @tensor C[u, o, l] := A1[i, l] * mps.A[1+half_N][u, i, o]
+    for n in 2:half_N-1
+        @tensor C[u, y, p] := C[u, o, l] * mps.A[n][l, j, p] * mps.A[n+half_N][o, j, y]
+    end
+    AN = mps.A[N][:, :, 1]
+    @tensor co = C[u, o, l] * mps.A[half_N][l, j, u] * AN[o, j]
+    return co
 end
 
-# %% DMRG sweep
+# %% parameters
 
 N = 100 # number of sites
 d = 2 # physical dim
-D = 20 # bond dim
-max_loops = 2
-E_Bethe = heisen_chain_Bethe(N, BC)
-
-Δ = 0.5
 BC = "PBC"
-mpo = xxz_chain_MPO(N, Δ, BC)
+Δ = 0.5
+mpo = xxz_chain_MPO(N, Δ, BC);
 
+# %% DMRG sweep
 
+max_loops = 3
+D = 80 # bond dim
+mps = MPS{Float64}(N, d, D)
+r2l_LQ!(mps)
+_ = DMRG_loop!(mps, mpo, max_loops, -1.)
+co = xxz_crosscap_overlay(mps)
+
+K_exact = xxz_luttinger_exact(Δ)
+K_dmrg = 1 / co^4
+
+println("Δ = $Δ, Exact K = ", K_exact)
+println("Δ = $Δ, DMRG  K = ", K_dmrg)
