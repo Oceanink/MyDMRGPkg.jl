dd91d7d16e6ebc609c57c44733751c1c6a7d6a65
Shen <oceanink2718@gmail.com>
2026-03-23 16:09:54 +0100

    add a new crosscap overlap

--- src/MPS/CrosscapOverlap.jl
+++ src/MPS/CrosscapOverlap.jl
@@ -14,3 +14,18 @@ function crosscap_overlap(mps::MPS)
     @tensor co = C[u, o, l] * mps.A[half_N][l, j, u] * AN[o, j]
     return co
 end
+
+function crosscap_overlap(mps::MPS, W)
+    N = mps.N
+    @assert N % 4 == 0
+
+    half_N = div(N, 2)
+    A1 = mps.A[1][1, :, :]
+    @tensor C[u, o, l] := A1[i, l] * W[i, j] * mps.A[1+half_N][u, j, o]
+    for n in 2:half_N-1
+        @tensor C[u, y, p] := C[u, o, l] * mps.A[n][l, i, p] * W[i, j] * mps.A[n+half_N][o, j, y]
+    end
+    AN = mps.A[N][:, :, 1]
+    @tensor co = C[u, o, l] * mps.A[half_N][l, i, u] * W[i, j] * AN[o, j]
+    return co
+end
